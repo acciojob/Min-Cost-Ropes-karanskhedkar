50ff1baa824cd705708a123cd19c6306b8306f8e
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-15 13:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr){ 
+	arr.sort((a,b)=>a-b);
+	let cost=0;
+	while(arr.length>1){
+		const first=arr[0];
+		const last=arr[1];
+		cost=cost+first+last;
+		arr.shift();
+		arr.shift();
+		arr.unshift(first+last);
+		arr.sort((a,b)=>a-b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
